87855de70136ac26f429ab14682083b10c09d5c2
Kalańska Emilia <kalanskae@gmail.com>
2024-12-05 09:52:01 +0100

    init prettier

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
 [Advent of code 2024](https://adventofcode.com/2024)
-
-[//]: <> (npm install eslint @typescript-eslint/parser @typescript-eslint/eslint-plugin -- save-dev)
-[//]: <> (npm i @types/node)
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,21 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+import 'eslint-plugin-only-warn';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
+  eslintPluginPrettier,
   ...tseslint.configs.recommended,
+  {
+    rules: {
+      endOfLine: 'off',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+    },
+  },
 ];
