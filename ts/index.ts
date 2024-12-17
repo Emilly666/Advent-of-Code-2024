2cd73a1e20cf5b4d030f7311fe59b38ce57e52ee
Kalańska Emilia <kalanskae@gmail.com>
2024-12-17 12:34:53 +0100

    wip

--- ts/index.ts
+++ ts/index.ts
@@ -1,9 +1,9 @@
-import { part1, part2 } from './src/day11';
+import { part1, part2 } from './src/day17';
 import * as cowsay from 'cowsay';
 
 console.log(
   cowsay.say({
-    text: part2().toString(),
+    text: part1().toString(),
     e: 'oo',
     T: 'U',
   }),
---
+++ ts/src/day17.ts
@@ -0,0 +1,105 @@
+import { exit } from 'node:process';
+import { readFile } from '../utils.js';
+
+const registers = {
+  A: 0,
+  B: 0,
+  C: 0,
+};
+
+export function part1() {
+  const input = readFile('input.txt').split('\r\n\r\n');
+  const top = input[0].split('\r\n');
+  registers.A = Number(top[0].split(': ')[1]);
+  registers.B = Number(top[1].split(': ')[1]);
+  registers.C = Number(top[2].split(': ')[1]);
+  const program = input[1]
+    .split(': ')[1]
+    .split(',')
+    .map((item) => Number(item));
+  const out: number[] = [];
+  let pointer = 0;
+  while (pointer < program.length) {
+    let inc = true;
+    const comboOperand = getCombo(program[pointer + 1]);
+    const literalOperand = program[pointer + 1];
+    switch (program[pointer]) {
+      case 0: {
+        //adv
+        registers.A = Math.trunc(registers.A / Math.pow(2, comboOperand));
+        break;
+      }
+      case 1: {
+        //bxl
+        registers.B = registers.B ^ literalOperand;
+        break;
+      }
+      case 2: {
+        //bst
+        registers.B = comboOperand % 8;
+        break;
+      }
+      case 3: {
+        //jnz
+        if (registers.A != 0) {
+          pointer = literalOperand;
+          inc = false;
+        }
+        break;
+      }
+      case 4: {
+        //bxc
+        registers.B = registers.B ^ registers.C;
+        break;
+      }
+      case 5: {
+        //out
+        out.push(comboOperand % 8);
+        break;
+      }
+      case 6: {
+        //bdv
+        registers.B = Math.trunc(registers.A / Math.pow(2, comboOperand));
+        break;
+      }
+      case 7: {
+        //cdv
+        registers.C = Math.trunc(registers.A / Math.pow(2, comboOperand));
+        break;
+      }
+    }
+    if (inc) {
+      pointer += 2;
+    }
+  }
+
+  return out.toString();
+}
+
+export function part2() {
+  const input = readFile('input.txt').split('\r\n\r\n');
+  const program = input[1]
+    .split(': ')[1]
+    .split(',')
+    .map((item) => Number(item))
+    .reverse()
+    .join('');
+  console.log(program)
+  const out = 8 * parseInt(program, 8);
+
+  return out;
+}
+
+function getCombo(n: number) {
+  if (n >= 0 && n <= 3) {
+    return n;
+  } else if (n == 4) {
+    return registers.A;
+  } else if (n == 5) {
+    return registers.B;
+  } else if (n == 6) {
+    return registers.C;
+  } else {
+    exit(1);
+  }
+}
